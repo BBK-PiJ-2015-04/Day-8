1a46ff176ffb5c2d1fde372e883f0ecb29c2d6a5
HS <none@email.none>
2015-11-22 14:56:56 +0000

    .

---
+++ 05 Maps/HashUtilities.java
@@ -0,0 +1,16 @@
+public class HashUtilities {
+	
+	public static void main(String[] args) {
+		while(true){
+			
+		System.out.println("Give me a string and I will calculate its hash code");
+		String str = System.console().readLine();
+		int hash = str.hashCode();
+		//int smallHash = HashUtilities.shortHash(hash);
+		System.out.println("0 < " + hash + " < 1000");
+		}
+		
+	}
+	
+	
+}
